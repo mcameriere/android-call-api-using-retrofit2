7359bd24d3c787e3f659d189190e6e71683416d2
mcameriere <31536331+mcameriere@users.noreply.github.com>
2019-10-20 21:16:04 +0200

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,11 @@
+package com.example.callapi;
+
+public class Player {
+    String firstName;
+    String lastName;
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
